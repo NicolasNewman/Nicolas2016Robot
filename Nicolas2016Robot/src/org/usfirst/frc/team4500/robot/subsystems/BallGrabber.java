a6a922103f993d1a77e2ddfaf0b3a89794b1c70c
BuildTools <unconfigured@null.spigotmc.org>
2016-04-11 10:37:15 -0500

    First Commit

---
+++ Nicolas2016Robot/src/org/usfirst/frc/team4500/robot/subsystems/BallGrabber.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team4500.robot.subsystems;
+
+import org.usfirst.frc.team4500.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class BallGrabber extends Subsystem {
+	
+	private DoubleSolenoid loader;
+	
+	public BallGrabber() {
+		loader = new DoubleSolenoid(RobotMap.LOAD_SOLENOID_1, RobotMap.LOAD_SOLENOID_2);
+	}
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void up() {
+    	loader.set(DoubleSolenoid.Value.kForward);
+    }
+    
+    public void down() {
+    	loader.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+    public enum loadDir {
+    	UP, DOWN
+    }
+    
+    public void load(loadDir dir) {
+    	if(dir == loadDir.UP) {
+    		up();
+    	} else {
+    		down();
+    	}
+    }
+}
+
